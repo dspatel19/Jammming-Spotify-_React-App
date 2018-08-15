256a0eeaf09c3e63198ac861a379237cffdc16e9
Dhaval Patel <dhaval96patel@gmail.com>
2018-08-14 20:31:54 -0500

    Work in Progress Jammming App(Update 3)

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component{
     super(props);
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
-    
+
   }
 
 
@@ -31,7 +31,7 @@ class SearchBar extends React.Component{
       <div className="SearchBar">
         <input placeholder="Enter A Song, Album, or Artist"
          onChange = {this.handleTermChange}/>
-        <a>SEARCH</a>
+        <a onClick= {this.search}>SEARCH</a>
       </div>
     );
   }
